194c51aed88cdcf468264cb39c6e4a22acb8f10b
sabcri9090 <sabogalcris6@gmail.com>
2026-01-28 10:34:26 -0500

    Add files via upload

---
+++ GBC gamepro/Exercise0/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, let me calc 4 u!");
+Console.WriteLine("enter a number");
+string answer = Console.ReadLine();
+int number1 = int.Parse(answer);
+Console.WriteLine("enter another number");
+answer = Console.ReadLine();
+int number2 = int.Parse(answer);
+int sum = 
+Console.WriteLine($"the sum of {number1} and {number2} is " + (number1 + number2));
