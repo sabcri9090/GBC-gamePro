a23cef1ca9003f4a3e4af6a894fec3521894a2b4
sabcri9090 <sabogalcris6@gmail.com>
2026-01-30 22:29:31 -0500

    Update Exercise1.cs
    added parse

--- Exercise1.cs
+++ Exercise1.cs
@@ -9,8 +9,10 @@ namespace MyApplication
       Console.WriteLine("hello, what is your name ");
       string name = Console.ReadLine();
 
+      int convert;
       Console.WriteLine("how old are you ");
       int age = Console.ReadLine();
+      convert = int.Parse(age);
 
       Console.WriteLine($"Hello, {name}! You are {age} years old.");
      }
